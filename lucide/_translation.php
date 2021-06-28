1e41d051ed27563fd7c569b5147195cc65e9f963
Simon Vandaele <simonvdfr@users.noreply.github.com>
2021-06-28 19:35:18 +0200

    archivage futur ?

---
+++ lucide/_translation.php
@@ -0,0 +1,157 @@
+<?php
+$add_translation = array(
+	"404 error : page not found" => array("fr" => "Erreur 404 : page introuvable"),
+	"Under Construction" => array("fr" => "En construction"),
+
+	"Search" => array("fr" => "Recherche"),
+	"Next" => array("fr" => "Suivant"),
+	"Previous" => array("fr" => "Précédent"),
+
+	"Text" => array("fr" => "Texte"),
+
+	"Log in" => array("fr" => "Connexion"),
+	"Administrator Login" => array("fr" => "Connexion Administrateur"),
+	"Connection with" => array("fr" => "Connexion avec"),
+	"Connection error" => array("fr" => "Erreur de connexion"),
+	"Token error" => array("fr" => "Erreur de jeton"),
+	"Password error" => array("fr" => "Erreur de mot de passe"),
+	"Unknown user" => array("fr" => "Utilisateur inconnu"),
+	"Unable to find the user number" => array("fr" => "Impossible de trouver le numéro d'utilisateur"),
+	"Unable to find the access token" => array("fr" => "Impossible de trouver le jeton d'accès"),
+	"Bad credential" => array("fr" => "Vous n'avez pas le niveau d'accès requis"),
+	"Connection required" => array("fr" => "Connexion requise"),
+	"You are already connected" => array("fr" => "Vous êtes déjà connecté"),
+
+	"Validate the connection in the popup" => array("fr" => "Validez la connexion dans la fenêtre qui vient de s'ouvrir"),
+
+	"My email" => array("fr" => "Mon e-mail"),
+	"My password" => array("fr" => "Mon mot de passe"),
+	"Remember me" => array("fr" => "Se souvenir de moi"),
+	"Forgot your password" => array("fr" => "Mot de passe oublié"),
+	"Suggest a password" => array("fr" => "Suggérer un mot de passe"),
+	"Send password by mail" => array("fr" => "Envoyer le mot de passe par mail"),
+	"New Password" => array("fr" => "Nouveau mot de passe"),
+	"See password" => array("fr" => "Voir le mot de passe"),
+	"Not a member yet ?" => array("fr" => "Pas encore membre ?"),
+	"Sign up" => array("fr" => "Inscrivez-vous"),
+
+
+	"List of contents" => array("fr" => "Liste des contenus"),
+
+	
+	"Add content" => array("fr" => "Ajouter un contenu"),
+	"Add page" => array("fr" => "Ajouter une page"),
+	"Add article" => array("fr" => "Ajouter un article"),
+	"Add event" => array("fr" => "Ajouter un événement"),
+	"Add media" => array("fr" => "Ajouter un média"),
+	"Add video" => array("fr" => "Ajouter une vidéo"),
+	"Add product" => array("fr" => "Ajouter un produit"),
+
+	"Add Item" => array("fr" => "Ajouter un élément"),
+
+
+	"Title" => array("fr" => "Titre"),
+	"Select template" => array("fr" => "Sélectionner un modèle"),
+	"Permanent link" => array("fr" => "Lien Permanent"),
+	"Regenerate address" => array("fr" => "Regénérer l'adresse"),
+	"Permanent link: 'index' if homepage" => array("fr" => "Lien permanent: 'index' si c'est la page d'accueil"),
+	"No permanent link for content" => array("fr" => "Pas de lien permanent pour le contenu"),
+
+
+	"Page title" => array("fr" => "Titre de la page"),
+	"Description for search engines" => array("fr" => "Description pour les moteurs de recherche"),
+	"Formatted web address" => array("fr" => "Adresse web formaté"),
+	
+	"Template" => array("fr" => "Modèle de page"),
+	"Type of page" => array("fr" => "Type de page"),
+	"Creation date" => array("fr" => "Date de création"),
+	
+	"Image on social networks" => array("fr" => "Image sur les réseaux sociaux"),
+	
+	"Close the edit mode" => array("fr" => "Fermer le mode d'édition"),
+	"Activation status" => array("fr" => "Etat d'activation"),
+
+
+	"Home page" => array("fr" => "Page d'accueil"),
+
+
+	"Media Library" => array("fr" => "Bibliothèque des médias"),
+	"Drag and drop a file here or click me" => array("fr" => "Glisser-déplacer un fichier ici ou cliquez-moi"),
+	"Delete file" => array("fr" => "Supprimer le fichier"),
+	"Media" => array("fr" => "Médias"),
+	"Images" => array("fr" => "Images"),
+	"Resized" => array("fr" => "Redimensionnées"),
+	"Files" => array("fr" => "Fichiers"),
+	"Specific" => array("fr" => "Spécifique"),
+	"Videos" => array("fr" => "Vidéos"),
+	"Audios" => array("fr" => "Audios"),
+	"Get resized image" => array("fr" => "Obtenir une image redimensionnée"),
+
+	"Size of source file unspecified" => array("fr" => "Taille du fichier source non précisée"),
+	"Unsupported file type" => array("fr" => "Type de fichier non pris en charge"),
+	
+	"Icon Library" => array("fr" => "Bibliothèque d'icône"),
+
+
+	"Loading" => array("fr" => "Chargement"),
+
+
+	"Save" => array("fr" => "Enregistrer"),
+	"Add" => array("fr" => "Ajouter"),
+	"Delete" => array("fr" => "Supprimer"),
+
+
+	"Register" => array("fr" => "S'inscrire"),
+	"Account created" => array("fr" => "Compte créé"),
+	"User deleted" => array("fr" => "Utilisateur supprimé"),
+
+	"Add user" => array("fr" => "Ajouter un utilisateur"),
+	"List of user" => array("fr" => "Liste des utilisateur"),
+	"My profil" => array("fr" => "Mon profil"),
+
+	"Your profile" => array("fr" => "Votre profil"),
+	"Profile" => array("fr" => "Profil"),
+
+	"State" => array("fr" => "Etat"),
+	"Authorization" => array("fr" => "Autorisation"),
+	"Name" => array("fr" => "Pseudo"),
+	"Password" => array("fr" => "Mot de passe"),
+	"Updated the" => array("fr" => "Mise à jour le"),
+	"Add the" => array("fr" => "Ajouté le"),
+
+	"Active" => array("fr" => "Actif"),
+	"Moderate" => array("fr" => "Modérer"),
+	"User email" => array("fr" => "Mail utilisateur"),
+	"Blacklist" => array("fr" => "Liste noire"),
+	"Deactivate" => array("fr" => "Désactivé"),
+
+	"New user to activate" => array("fr" => "Nouvel utilisateur a activé"),
+	"User profile" => array("fr" => "Profil de l'utilisateur"),
+
+
+	"Managing admins" => array("fr" => "Gestion des admins"),
+	"Managing users" => array("fr" => "Gestion des utilisateurs"),
+
+	"Edit Config" => array("fr" => "Modifier la config"),
+	
+	"Edit menu" => array("fr" => "Edition du menu"),
+	"Edit header" => array("fr" => "Modifier l'en-tête"),
+	"Edit footer" => array("fr" => "Modifier le pied de page"),
+
+	"Send Files" => array("fr" => "Envoyer des fichiers"),
+	"Edit Files" => array("fr" => "Modifier les fichiers"),
+
+	"Edit page" => array("fr" => "Edition des pages"),
+	"Edit article" => array("fr" => "Edition des articles"),
+	"Edit product" => array("fr" => "Edition des produits"),
+	"Edit video" => array("fr" => "Edition des vidéo"),
+
+	"Public content" => array("fr" => "Contenu public"),
+	"Public file" => array("fr" => "Dossier public"),
+
+	"Page deleted, redirecting" => array("fr" => "Page supprimée, redirection en cours"),
+
+
+	"Log out" => array("fr" => "Se déconnecter")
+);
+?>
